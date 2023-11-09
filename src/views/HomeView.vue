6b10fb5fdb18df37cb40d7cfa7b90ba486329fb9
Angel Isaac Fonseca Gómez <L201050207@pabellon.tecnm.mx>
2023-11-09 16:44:38 -0600

    Frontend de Creditos
    Frontend proyecto creditos

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import {Logged} from '../router/index.js'
+import BaseLayout from '../layout/BaseLayout.vue'
+// Logged()
+</script>
+
+<template>
+  <BaseLayout>
+    <main>
+      <h1 class="text-center">Bienvenido a Gestion de créditos ITPA</h1>
+    </main>
+  </BaseLayout>
+</template>
+
+
